931fc66dfb0345ec531199fe84d48944f281c7bd
fengzihao1013 <958608363@qq.com>
2023-03-18 20:54:34 +0800

    考试中页面完成

--- Opening part/quesionBank_font/pages/stuPart/examing/examing.js
+++ Opening part/quesionBank_font/pages/stuPart/examing/examing.js
@@ -5,8 +5,135 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        show: false,
+        is_danxuan:false,
+        is_duoxuan:false,
+        is_jisuan:false,
+        is_tiankong:true,
+        is_wenda:false,
+        is_biancheng:false
     },
+    // 获取答案
+    getAnswer(event){
+        console.log(event.detail.value)
+    },
+    // 点击单选
+    select_danxuan(){
+        let is_duoxuan = false
+        let is_jisuan = false
+        let is_tiankong = false
+        let is_wenda = false
+        let is_biancheng = false
+        let is_danxuan = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+
+    // 点击多选
+    select_duoxuan(){
+        let is_danxuan = false
+        let is_jisuan = false
+        let is_tiankong = false
+        let is_wenda = false
+        let is_biancheng = false
+        let is_duoxuan = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+    // 点击填空
+    select_tiankong(){
+        let is_danxuan = false
+        let is_jisuan = false
+        let is_duoxuan = false
+        let is_wenda = false
+        let is_biancheng = false
+        let is_tiankong = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+     // 点击计算
+     select_jisuan(){
+        let is_danxuan = false
+        let is_tiankong = false
+        let is_duoxuan = false
+        let is_wenda = false
+        let is_biancheng = false
+        let is_jisuan = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+    // 点击问答
+    select_wenda(){
+        let is_danxuan = false
+        let is_tiankong = false
+        let is_duoxuan = false
+        let is_jisuan = false
+        let is_biancheng = false
+        let is_wenda = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+    // 点击编程
+    select_biancheng(){
+        let is_danxuan = false
+        let is_tiankong = false
+        let is_duoxuan = false
+        let is_jisuan = false
+        let is_wenda = false
+        let is_biancheng = true
+        this.setData({
+            is_danxuan,
+            is_duoxuan,
+            is_tiankong,
+            is_jisuan,
+            is_wenda,
+            is_biancheng
+        })
+        //this.onShow()
+    },
+
+    // 点击全部
+    quanbu(){
+        this.setData({ show: true });
+    },   
+    onClose() {
+        this.setData({ show: false });
+      },
 
     /**
      * 生命周期函数--监听页面加载
@@ -26,7 +153,6 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
     },
 
     /**
