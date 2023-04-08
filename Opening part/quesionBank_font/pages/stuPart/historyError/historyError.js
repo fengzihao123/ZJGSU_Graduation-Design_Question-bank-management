bc7760a006742e440b4b23ae0a0e0d354f425e97
fengzihao1013 <958608363@qq.com>
2023-04-08 13:31:49 +0800

    错题及相关接口

--- Opening part/quesionBank_font/pages/stuPart/historyError/historyError.js
+++ Opening part/quesionBank_font/pages/stuPart/historyError/historyError.js
@@ -1,6 +1,7 @@
 // pages/stuPart/historyError/historyError.js
 import Dialog from '@vant/weapp/dialog/dialog';
 import request from '../../../utils/request'
+import upload from '../../../utils/upload'
 Page({
 
     /**
@@ -9,165 +10,94 @@ Page({
     data: {
         index:0,
         answer:[],
+        stuInfo:[],
         temp:'',
-        result: ['a', 'b'],
         flageA:false,
         flageB:false,
         flageC:false,
         flageD:false,
         temp:'',
-        collectList:[
-            {
-                "queId": "1324612474954258",
-                "queType": "单选",
-                "stem": "基家将步族发必细眼青厂求资多那。调却展利方集样打红变值来始府做听观。我成其动示指就解白把马般育引资会天。织重机十增用率军机加议容形还或听回林。存不拉些会即种两在间压素入响。",
-                "choiceA": "器层革带面况万果子计六观文。",
-                "choiceB": "少任里结省调总问意位革传江它立工选。",
-                "choiceC": "目快斗系取今车了光选千界。",
-                "choiceD": "其强光采家计队期多办道示存级市。",
-                "chaName": "第2章",
-                "difficulty": "1",
-                "answer": "超",
-                "explain": "联些选级政存不队命两级维直。心向边与二立往者名住众多今提从。据较最现不力安度历手反派么。心万机参做及越起论论去率员之法面八。术县儿温由算得它率是然马本增要。农已压院便据起九相列如命认。",
-                "isCollect": "true",
-                "is_pcik":"true",
-                "myAnswer":"A"
-            },
-            {
-                "queId": "881823182184393",
-                "queType": "多选",
-                "stem": "记角教门现今争响地将育治转十问角持。非越实再许信计断对每提象论林。她眼需并规存般究儿克律器查。此识民向政发给军半过空据即少音。",
-                "choiceA": "算质同少会受农意器劳取采米常温支。",
-                "choiceB": "什铁已所史亲义自求名音劳北。",
-                "choiceC": "果反离近由青现属出或面声走眼与际。",
-                "choiceD": "只制体响酸五安产要受也传联好何及。",
-                "chaName": "第1章",
-                "difficulty": "5",
-                "answer": "强",
-                "explain": "准先说究万党问类布物议单地切合老。子书克水表取划育次准社性县指解地。但调次但却路周热细代求产月般层已快节。王并且面事场第他达面用实九的满同革。可边自内物体进且每技太高报油许使。口所关况门些性例每马文消化科美整。很林太结主认会见快心关解。",
-                "isCollect": "true",
-                "is_pcik":"false",
-                "myAnswer":"AB"
-            },
-            {
-                "queId": "4455535709600350",
-                "queType": "填空",
-                "stem": "状几变程团常资七周意支毛重史可得具。行类商研价体科理太志象变放化细。且它何经设五领科间第教状包使起满。样写道林六活历空外不据权手听手气马。行道日命政资率候此备真该历计近保习。发何十时回农象边号山器重因记几常。",
-                "choiceA": null,
-                "choiceB": null,
-                "choiceC": null,
-                "choiceD": null,
-                "chaName": "第2章",
-                "difficulty": "4",
-                "answer": "军",
-                "explain": "这南先向联书方就和化识酸广。强清已约六多加格中记书委。此断张八观利动采科家响广认科条设没断。",
-                "isCollect": "true",
-                "is_pcik":"false",
-                "myAnswer":"16"
-            },
-            {
-                "queId": "8396874067531420",
-                "queType": "计算",
-                "stem": "对成而细你好中劳四还门了安。红用式构别装色而群及思事物开千快素。看传小任响你场油主前信住没期王。复来性水写较切话细列性步列。",
-                "choiceA": null,
-                "choiceB": null,
-                "choiceC": null,
-                "choiceD": null,
-                "chaName": "第2章",
-                "difficulty": "2",
-                "answer": "娜",
-                "explain": "比形示易共受感色约间生求后完清。第需入观海新克难种需方类采路权务政原。到飞种料象造己海小观及新强资革。单学般论育前里儿物风育入号义南精消布。",
-                "isCollect": "true",
-                "is_pcik":"false",
-                "myAnswer":"是的"
-            },
-            {
-                "queId": "390807626540014",
-                "queType": "问答",
-                "stem": "力取根图产济成最用想红事万单却于济种。这技指很权水证任理新自利听复毛。就写达子现书深王口命年统心查。广何开二党意土般县身那正由须候来。示般热是复战还统边准道很此别圆间增。基今影反相她但用几片院消放规己劳。",
-                "choiceA": null,
-                "choiceB": null,
-                "choiceC": null,
-                "choiceD": null,
-                "chaName": "第5章",
-                "difficulty": "3",
-                "answer": "娟",
-                "explain": "走拉高具青铁事响里两两相了深于定。验工造主断教住出并动实社行型商较声般。它算周音济资然效选型过达以族北。资革原算前族即气工此离声形。者很又事感质华县写律道持身题历常。光么各连强车较整采许儿式产构识体近程。",
-                "isCollect": "true",
-                "is_pcik":"false",
-                "myAnswer":"A"
-            },
-            {
-                "queId": "390807626540014",
-                "queType": "编程",
-                "stem": "力取根图产济成最用想红事万单却于济种。这技指很权水证任理新自利听复毛。就写达子现书深王口命年统心查。广何开二党意土般县身那正由须候来。示般热是复战还统边准道很此别圆间增。基今影反相她但用几片院消放规己劳。",
-                "choiceA": null,
-                "choiceB": null,
-                "choiceC": null,
-                "choiceD": null,
-                "chaName": "第5章",
-                "difficulty": "3",
-                "answer": "娟",
-                "explain": "走拉高具青铁事响里两两相了深于定。验工造主断教住出并动实社行型商较声般。它算周音济资然效选型过达以族北。资革原算前族即气工此离声形。者很又事感质华县写律道持身题历常。光么各连强车较整采许儿式产构识体近程。",
-                "isCollect": "true",
-                "is_pcik":"false",
-                "myAnswer":"A"
-            }
-        
-        ],
+        errorList:[],
         show: false,
     },
 
-
-    // 收藏
-    collect(){
-        let index = this.data.index
-        let collectList = this.data.collectList
-        collectList[index].isCollect = true
-        this.setData({
-            collectList
-        })
-
-    },
-    //取消收藏
-    no_collect(){
-        let index = this.data.index
-        let collectList = this.data.collectList
-        var that = this
-        wx.showModal({
-            title: '提示',
-            content: '确认取消收藏吗？',
-            success (res) {
-              if (res.confirm) {
-                collectList[index].isCollect = false
-                that.setData({
-                    collectList
-                })
-              } else if (res.cancel) {
-              }
-            }
-          })
-       
-    },
+     
     //删除
     shanchu(){
         let index = this.data.index
-        let collectList = this.data.collectList
+        let errorList = this.data.errorList
         var that = this
         wx.showModal({
             title: '提示',
             content: '确认删除吗？',
             success (res) {
               if (res.confirm) {
-                collectList[index].isCollect = false
+                that.deleteError()
+                let newError = that.getErrorList(that.data.stuInfo[0].stuId)
                 that.setData({
-                    collectList
+                    errorList:newError
                 })
               } else if (res.cancel) {
               }
             }
           })
     },
+
+     // 收藏
+     collect(){
+        let index = this.data.index
+        let errortList = this.data.errorList
+        var that = this
+        wx.showModal({
+            title: '提示',
+            content: '确认收藏吗？',
+            success (res) {
+              if (res.confirm) {
+                that.Collect()
+                errortList.data[index].isCollect = 1
+                that.setData({
+                    errorList
+                })
+              } else if (res.cancel) {
+              }
+            }
+          })
+
+    },
+    //  //收藏函数
+    //  async Collect(){
+    //     let index = this.data.index
+    //     let errorList = this.data.errorList
+    //     await upload('/question/collect/collect?stuId=' + collectList.data[index].stuId + '&queId=' + collectList.data[index].queId,{isCollect:1})
+    // },
+    // //取消收藏函数
+    // async cancleCollect(){
+    //     let index = this.data.index
+    //     let collectList = this.data.collectList
+    //     await upload('/question/collect/deleteCollection?stuId=' + collectList.data[index].stuId + '&queId=' + collectList.data[index].queId,{isCollect:0})
+    // },
+    // //取消收藏
+    // async no_collect(){
+    //     let index = this.data.index
+    //     let collectList = this.data.collectList
+    //     var that = this
+    //     wx.showModal({
+    //         title: '提示',
+    //         content: '确认取消收藏吗？',
+    //         success (res) {
+    //           if (res.confirm) {
+    //             that.cancleCollect()
+    //             collectList.data[index].isCollect = 0
+    //             that.setData({
+    //                 collectList
+    //             })
+    //           } else if (res.cancel) {
+    //           }
+    //         }
+    //       })
+    // },
+
+
     // 点击全部
     quanbu(){
         this.setData({ show: true });
@@ -177,64 +107,77 @@ Page({
       },
 
       //错题列表获取
-      async getCollectList(){
-          let collectList = await request('/question/collect/getCollection',{stuId:'1911060118'})
+      async getErrorList(stuId){
+          let errorList = await request('/question/errorQuestion/getError',{stuId:stuId})
           this.setData({
-              collectList
+              errorList
           })
+      },
+      //错题
+    //   async Error(){
+    //     let index = this.data.index
+    //     let errorList = this.data.errorList
+    //     let result = await upload('/question/errorQuestion/error?stuId=' + errorList.data[index].stuId + '&queId=' + errorList.data[index].queId,{isError:1})
+    //     console.log(result)
+    //   },
+      //删除错题
+      async deleteError(){
+          let index = this.data.index
+          let errorList = this.data.errorList
+          let result = await upload('/question/errorQuestion/deleteError?stuId=' + errorList.data[index].stuId + '&queId=' + errorList.data[index].queId,{isError:0})
+          console.log(result)
       },
 
       // 查看index
       chageIndex(event){
           let index = event.currentTarget.dataset
-          let collect = this.data.collectList
-          for(var i = 0;i < collect.length;i++){
-             collect[i].is_pcik = 'false'
+          let error = this.data.errorList
+          for(var i = 0;i < error.data.length;i++){
+             error.data[i].isPick = 'false'
           }
-          collect[index.index].is_pcik = 'true'
+          error.data[index.index].isPick = 'true'
           this.setData({
               index:index.index,
-              collectList:collect,
+             errorList:error,
               show:false
           })
       },
       //上一题
       toPre(){
           let index = this.data.index
-          let length = this.data.collectList.length
-          let collectList = this.data.collectList
+          let length = this.data.errorList.data.length
+          let errorList = this.data.errorList
           if(index == 0){
               index = length - 1 
           }else{
               index = index - 1
           }
-          for(var i = 0;i < collectList.length;i++){
-            collectList[i].is_pcik = 'false'
+          for(var i = 0; i <errorList.data.length; i++){
+            errorList.data[i].isPick = 'false'
           }
-          collectList[index].is_pcik = 'true'
+          errorList.data[index].isPick = 'true'
           this.setData({
               index:index,
-              collectList,
+              errorList,
           })
       },
       //下一题
       toNext(){
         let index = this.data.index
-        let length = this.data.collectList.length
-        let collectList = this.data.collectList
+        let length = this.data.errorList.data.length
+        let errorList = this.data.errorList
         if(index == length - 1){
             index = 0
-    
         }else{
             index = index + 1
         }
-        for(var i = 0;i < collectList.length;i++){
-            collectList[i].is_pcik = 'false'
+        for(var i = 0;i < errorList.data.length;i++){
+            errorList.data[i].isPick = 'false'
           }
-          collectList[index].is_pcik = 'true'
+          errorList.data[index].isPick = 'true'
           this.setData({
               index:index,
-              collectList,
+              errorList,
           })
         
     },
@@ -244,7 +187,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        
+        let stuInfo = wx.getStorageSync('stuInfo')
+        this.setData({
+            stuInfo
+        })
+        this.getErrorList(stuInfo[0].stuId)
     },
 
     /**
